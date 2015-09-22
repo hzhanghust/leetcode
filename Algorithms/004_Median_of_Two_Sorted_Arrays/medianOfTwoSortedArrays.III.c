429b7469e3ecf5d3f8ac8100cf730f3fc960fd85
hzhanghust <hzhang.hust@gmail.com>
2015-09-22 10:50:34 +0800

    Change Recursive to Iteration.

---
+++ Algorithms/004_Median_of_Two_Sorted_Arrays/medianOfTwoSortedArrays.III.c
@@ -0,0 +1,98 @@
+/*************************************************************************
+	> File Name: medianOfTwoSortedArrays.c
+	> Author: Hans
+	> Mail: hzhang.hust@gmail.com
+	> Created Time: Fri 11 Sep 2015 04:20:18 PM CST
+ ************************************************************************/
+
+/*
+ * There are two sorted arrays nums1 and nums2 of size m and n respectively. 
+ * Find the median of the two sorted arrays. The overall run time complexity 
+ * should be O(log (m+n)).
+ */
+
+#include<stdio.h>
+
+/* 
+ * [notice]
+ *   change Recursive to Iterative.
+ */
+double findKth_NoRecursive(int *nums1, int nums1Size, int *nums2, int nums2Size, int k) {
+	int pa, pb, tmpSize;
+	int *tmpNum;
+	
+	if (nums1Size > nums2Size) {
+		tmpNum = nums1, nums1 = nums2, nums2 = tmpNum;
+		tmpSize = nums1Size, nums1Size = nums2Size, nums2Size = tmpSize;
+	}
+
+	while (nums1Size != 0 && k != 1) {
+		pa = nums1Size > k/2 ? k/2 : nums1Size;
+		pb = k - pa;
+		if (nums1[pa - 1] < nums2[pb - 1]) {
+			nums1 += pa;
+			nums1Size -= pa;
+			k -= pa;
+		} else if (nums1[pa - 1] > nums2[pb - 1]) {
+			nums2 += pb;
+			nums2Size -= pb;
+			k -= pb;
+		} else {
+			return nums1[pa - 1];
+		}
+		if (nums1Size > nums2Size) {
+			tmpNum = nums1, nums1 = nums2, nums2 = tmpNum;
+			tmpSize = nums1Size, nums1Size = nums2Size, nums2Size = tmpSize;
+		}
+	}
+
+	if (0 == nums1Size) {
+		return nums2[k - 1];
+	}
+	if (1 == k) {
+		return nums1[0] > nums2[0]? nums2[0] : nums1[0];
+	}
+}
+
+/* 
+ * find the k-th smallest elements of arrays nums1 & nums2
+ */
+double findKth(int *nums1, int nums1Size, int *nums2, int nums2Size, int k) {
+	int pa, pb;
+
+	/* This is must, otherwise pb may larger than nums2Size */
+	if (nums1Size > nums2Size) {
+		return findKth(nums2, nums2Size, nums1, nums1Size, k);
+	}
+
+	if (0 == nums1Size) {
+		return nums2[k - 1];
+	}
+	if (1 == k) {
+		return nums1[0] > nums2[0] ? nums2[0] : nums1[0];
+	}
+
+	pa = nums1Size > k/2 ? k/2 : nums1Size;
+	pb = k - pa;
+
+	if (nums1[pa - 1] < nums2[pb -1]) {
+		return findKth(nums1 + pa, nums1Size - pa, nums2, nums2Size, k - pa);
+	} else if (nums1[pa -1] > nums2[pb -1]) {
+		return findKth(nums1, nums1Size, nums2 + pb, nums2Size - pb, k - pb);
+	} else {
+		return nums1[pa -1];
+	}
+}
+
+double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size) {
+	int total;
+	
+	total = nums1Size + nums2Size;
+
+	if (total & 0x1) {
+		return findKth_NoRecursive(nums1, nums1Size, nums2, nums2Size, total/2 + 1);
+	} else {
+		return (findKth_NoRecursive(nums1, nums1Size, nums2, nums2Size, total/2)
+			+ findKth_NoRecursive(nums1, nums1Size, nums2, nums2Size, total/2 + 1))/2;
+	}
+}
--- Algorithms/004_Median_of_Two_Sorted_Arrays/test.c
+++ Algorithms/004_Median_of_Two_Sorted_Arrays/test.c
@@ -7,11 +7,11 @@
 
 #include<stdio.h>
 
-#include "medianOfTwoSortedArrays.II.c"
+#include "medianOfTwoSortedArrays.III.c"
 
 int main(void) {
-	int nums1[] = {2,4,4,6,8,10,11};
-	int nums2[] = {1,2,5,6};
+	int nums1[] = {100001};
+	int nums2[] = {100000};
 	int nums1Size, nums2Size, i;
 	double res;
 
@@ -41,9 +41,9 @@ int main(void) {
 	printf("output:\n");
 	printf("\t%lf\n", res);
 
-	printf("others:\n");
+	/* printf("others:\n");
 	printf("\t2-th-small is %d\n", (int)findKthSmall(nums1, nums1Size, nums2, nums2Size, 2));
 	printf("\t2-th-large is %d\n", (int)findKthLarge(nums1, nums1Size, nums2, nums2Size, 2));
 	printf("\t6-th-small is %d\n", (int)findKthSmall(nums1, nums1Size, nums2, nums2Size, 6));
-	printf("\t6-th-large is %d\n", (int)findKthLarge(nums1, nums1Size, nums2, nums2Size, 6));
+	printf("\t6-th-large is %d\n", (int)findKthLarge(nums1, nums1Size, nums2, nums2Size, 6)); */
 }
